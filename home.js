3b97ba998e2c1e8377f5e196ad89db9d4e76dd8f
Camilsrz <141527221+Camilsrz@users.noreply.github.com>
2023-11-30 23:16:21 -0500

    Add files via upload

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const cerrarsesion = document.getElementById("cerrar sesion")
 
 cerrarsesion.addEventListener("click", () => {
-   window.location = ('iniciar sesion.html')
+   window.location = ('index.html')
 })
 
 
